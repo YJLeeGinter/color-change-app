3cc7c4bdd870f23234203e4ff02b924a1e7082a2
Wiley <ginterlee29@gmail.com>
2021-06-05 02:39:23 -0400

    final commit

--- index.js
+++ index.js
@@ -6,14 +6,28 @@ const percentageBar = document.getElementById('percentageBar');
 const toggleBtnContainer = document.querySelector('.toggle-container');
 const toggleBtnBall = document.querySelector('.btn');
 
+
+const reset = () => {
+    alteredColorContainer.style.backgroundColor = '#' + userInput.value;
+    percentageBar.value = 0;
+    percentageText.innerText = '0%';
+
+};
+
 toggleBtnContainer.addEventListener('click', ()=>{
 
-toggleBtnBall.classList.add('toggled')
+    reset();
+    if(toggleBtnBall.classList.contains('toggled')){
+        toggleBtnBall.classList.remove('toggled');
+        
+    }else{
+        toggleBtnBall.classList.add('toggled');
+    }
 });
 
 // check if the user input is vailed 
 const isVaildHex = (hex) => {
-    if(!hex) return false;
+    if(!hex) return null;
 
     const strippedHex = hex.replace('#', '');
 
@@ -27,11 +41,14 @@ userInput.addEventListener('keyup', ()=> {
 
     const userInputHexValue = userInput.value.replace('#', '');
     inputColorContainer.style.backgroundColor = '#' + userInputHexValue;
+    reset();
 });
 
-// hex to rgb function
 
+// hex to rgb function
 const hexToRGB = (hex) => {
+
+    if(!hex) return;
 
     // 세글자이거나 여섯글자 
     // 세글자인경우 각 자리수를 한번 씩 중복
@@ -42,13 +59,14 @@ const hexToRGB = (hex) => {
 
     const RGBObj = {};
 
-    RGBObj.r = parseInt(hexValue[0]+hexValue[1] , 16);
-    RGBObj.g = parseInt(hexValue[2]+hexValue[3] , 16);
-    RGBObj.b = parseInt(hexValue[4]+hexValue[5] , 16);
+    RGBObj.r = parseInt(hexValue.substring(0,2) , 16);
+    RGBObj.g = parseInt(hexValue.substring(2,4) , 16);
+    RGBObj.b = parseInt(hexValue.substring(4,6) , 16);
 
     return RGBObj;
 
 };
+
 
 const RGBToHex = (RGBObj) => {
 
@@ -70,23 +88,36 @@ const RGBToHex = (RGBObj) => {
 
 }
 
+const changeColor = (hex, howMuchInput) => {
 
-const changeColor = (RGBObj, inputValue) => {
-    const alteredRGBObj = {};
-    alteredRGBObj.r = RGBObj.r + (inputValue/100) * 255;
-    alteredRGBObj.g = RGBObj.g + (inputValue/100) * 255;
-    alteredRGBObj.b = RGBObj.b + (inputValue/100) * 255;
+    const amount = Math.floor((howMuchInput/100) * 255);
+    console.log(amount);
+    const RGBObj = hexToRGB(hex);
 
-    const alteredHexColor = RGBToHex(alteredRGBObj);
-    console.log('just checking');
-    alteredColorContainer.style.backgroundColor = '#' + alteredHexColor;
+    RGBObj.r = limitIncreasedAmount(RGBObj.r, amount);
+    RGBObj.g = limitIncreasedAmount(RGBObj.g, amount);
+    RGBObj.b = limitIncreasedAmount(RGBObj.b, amount);
+    console.log(RGBObj);
+
+    const alteredHexColor = RGBToHex(RGBObj);
+    return alteredHexColor;
 }
+
+const limitIncreasedAmount = (hex, amount) => {
+    return Math.min(255, Math.max(0, hex + amount));
+} 
 
 // update the number after user adjust the range bar
 
 percentageBar.addEventListener('input', () => {
+    if(!isVaildHex(userInput.value)) return;
 
-    percentageText.innerText = `${percentageBar.value}%`
-    const rgbColor = hexToRGB(userInput.value);
-    changeColor(rgbColor, percentageBar.value);
+    percentageText.innerText = `${percentageBar.value}%`;
+
+    const howMuchInput = toggleBtnBall.classList.contains('toggled')?
+    -percentageBar.value : percentageBar.value; 
+    
+    const alteredHexColor = changeColor(userInput.value, howMuchInput);
+    alteredColorContainer.style.backgroundColor = '#' + alteredHexColor;
+
 });
